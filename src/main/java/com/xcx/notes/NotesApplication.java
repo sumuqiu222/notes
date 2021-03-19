17f9c8ee5b95f441c3e9a4d3a3671e153525ae26
sumuqiu222 <Woaiwojia152248>
2021-03-19 20:34:52 +0800

    项目初始化

--- src/main/java/com/xcx/notes/NotesApplication.java
+++ src/main/java/com/xcx/notes/NotesApplication.java
@@ -3,6 +3,9 @@ package com.xcx.notes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author xcx
+ */
 @SpringBootApplication
 public class NotesApplication {
 
